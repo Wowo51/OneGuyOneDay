b0a500b71ae94a2a8b2dac13a8a9756147920c13
Warren Harding <warrencharding@yahoo.com>
2025-02-24 01:30:32 -0700

    Add src/JacobiMethod/JacobiMethod/Program.cs

---
+++ src/JacobiMethod/JacobiMethod/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace JacobiMethod
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        // Entry point - no user interaction is provided.
+        }
+    }
+}
