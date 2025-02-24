ec9639a1d76a50b9c6edb78917775e6d1712c764
Warren Harding <warrencharding@yahoo.com>
2025-02-24 02:37:05 -0700

    Add src/LRParser/LRParser/Parser/Token.cs

---
+++ src/LRParser/LRParser/Parser/Token.cs
@@ -0,0 +1,12 @@
+namespace LRParser.Parser
+{
+    public class Token
+    {
+        public string Value { get; set; }
+
+        public Token(string value)
+        {
+            Value = value;
+        }
+    }
+}
