98b6888bfb97d5565b949f1438f1908368578c7c
Warren Harding <warrencharding@yahoo.com>
2025-02-24 02:19:59 -0700

    Add src/StochasticUniversalSampling/StochasticUniversalSamplingTest/CustomCandidate.cs

---
+++ src/StochasticUniversalSampling/StochasticUniversalSamplingTest/CustomCandidate.cs
@@ -0,0 +1,12 @@
+namespace StochasticUniversalSamplingTest
+{
+    public class CustomCandidate
+    {
+        public int Value { get; set; }
+
+        public CustomCandidate(int value)
+        {
+            Value = value;
+        }
+    }
+}
