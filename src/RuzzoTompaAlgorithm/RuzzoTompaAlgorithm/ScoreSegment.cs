5684007efd5efa2856a21d4cbbaa97305a62c6fc
Warren Harding <warrencharding@yahoo.com>
2025-02-24 02:08:11 -0700

    Add src/RuzzoTompaAlgorithm/RuzzoTompaAlgorithm/ScoreSegment.cs

---
+++ src/RuzzoTompaAlgorithm/RuzzoTompaAlgorithm/ScoreSegment.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RuzzoTompaAlgorithm
+{
+    public class ScoreSegment
+    {
+        public int Start { get; set; }
+        public int End { get; set; }
+        public double Score { get; set; }
+    }
+}
