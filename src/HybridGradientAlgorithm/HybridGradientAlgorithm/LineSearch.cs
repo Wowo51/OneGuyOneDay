8bca7b720ce070af3ef5c3df44f0224f7b5d4704
Warren Harding <warrencharding@yahoo.com>
2025-02-24 01:27:24 -0700

    Add src/HybridGradientAlgorithm/HybridGradientAlgorithm/LineSearch.cs

---
+++ src/HybridGradientAlgorithm/HybridGradientAlgorithm/LineSearch.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HybridGradientAlgorithm
+{
+    public class LineSearch
+    {
+        public double Perform(double[] candidate)
+        {
+            return 0.1;
+        }
+    }
+}
