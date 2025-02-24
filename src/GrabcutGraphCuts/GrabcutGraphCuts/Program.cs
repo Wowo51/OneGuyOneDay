f6163b41ebceac43f4193772f66a367deb0262a1
Warren Harding <warrencharding@yahoo.com>
2025-02-24 01:21:51 -0700

    Add src/GrabcutGraphCuts/GrabcutGraphCuts/Program.cs

---
+++ src/GrabcutGraphCuts/GrabcutGraphCuts/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace GrabcutGraphCuts
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            GrabcutSegmenter segmenter = new GrabcutSegmenter();
+            segmenter.Run();
+        }
+    }
+}
