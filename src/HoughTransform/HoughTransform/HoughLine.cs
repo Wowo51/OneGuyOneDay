5ad21fba6cd0324d7e0a3de3102bd8959726c3f6
Warren Harding <warrencharding@yahoo.com>
2025-02-24 01:25:53 -0700

    Add src/HoughTransform/HoughTransform/HoughLine.cs

---
+++ src/HoughTransform/HoughTransform/HoughLine.cs
@@ -0,0 +1,9 @@
+namespace HoughTransform
+{
+    public class HoughLine
+    {
+        public double Theta { get; set; }
+        public double Rho { get; set; }
+        public int Votes { get; set; }
+    }
+}
