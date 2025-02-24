9489ee9e05ee67d5518c9dcfec6f5588306897b5
Warren Harding <warrencharding@yahoo.com>
2025-02-24 02:37:26 -0700

    Add src/OddsAlgorithm/OddsAlgorithm/OddsAlgorithm/Program.cs

---
+++ src/OddsAlgorithm/OddsAlgorithm/OddsAlgorithm/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace OddsAlgorithm
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Simulator simulator = new Simulator();
+            simulator.RunSimulation();
+        }
+    }
+}
