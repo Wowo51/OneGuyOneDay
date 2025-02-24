652acc66e0a2ea10094bb51c45aee5176c8ddf42
Warren Harding <warrencharding@yahoo.com>
2025-02-24 01:44:30 -0700

    Add src/Mae1/Mae1/Program.cs

---
+++ src/Mae1/Mae1/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Mae1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MaeApplication.Run();
+        }
+    }
+}
