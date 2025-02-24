9338d406d8abeb390ef86327aa878b1e85645f3f
Warren Harding <warrencharding@yahoo.com>
2025-02-24 01:24:25 -0700

    Add src/HindleyMilnerAlgorithm/HindleyMilnerAlgorithm/Program.cs

---
+++ src/HindleyMilnerAlgorithm/HindleyMilnerAlgorithm/Program.cs
@@ -0,0 +1,10 @@
+namespace HindleyMilnerAlgorithm
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+        // Entry point: no test code provided.
+        }
+    }
+}
