5a6dfe0b3ef4ae0f695b443151124a1207278709
Warren Harding <warrencharding@yahoo.com>
2025-02-24 01:10:51 -0700

    Add src/EvolutionStrategy/EvolutionStrategy/Program.cs

---
+++ src/EvolutionStrategy/EvolutionStrategy/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace EvolutionStrategy
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            EvolutionStrategyAlgorithm algorithm = new EvolutionStrategyAlgorithm();
+            algorithm.Run();
+        }
+    }
+}
