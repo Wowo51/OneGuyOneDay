3397b878658c14b77f4ad5a576f803b10c0ba478
Warren Harding <warrencharding@yahoo.com>
2025-02-24 01:00:45 -0700

    Add src/CuttingPlaneMethod/CuttingPlaneMethod/Program.cs

---
+++ src/CuttingPlaneMethod/CuttingPlaneMethod/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace CuttingPlaneMethod
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CuttingPlaneSolver solver = new CuttingPlaneSolver();
+            solver.Solve();
+        }
+    }
+}
